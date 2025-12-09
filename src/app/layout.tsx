a7cc7debe8b3fb299295dd76832f944b6c14c56e
Leonardo <capacitacionvinoqro@gmail.com>
2025-12-09 16:41:40 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: useAuth must be

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,17 +22,17 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased min-h-screen bg-background">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <FirebaseClientProvider>
-            {children}
-          </FirebaseClientProvider>
-          <Toaster />
-        </ThemeProvider>
+        <FirebaseClientProvider>
+            <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+            >
+                {children}
+                <Toaster />
+            </ThemeProvider>
+        </FirebaseClientProvider>
       </body>
     </html>
   );
