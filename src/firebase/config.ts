2c3979321ad54f4fb72343faeafd4997d4c7e379
Leonardo <capacitacionvinoqro@gmail.com>
2025-12-09 19:21:22 +0000

    Fix de bucket

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyD6wcpIQvXfSX_Wq7lHlmkA4eCSI2nLozQ",
   authDomain: "studio-4910049931-ac9b9.firebaseapp.com",
   projectId: "studio-4910049931-ac9b9",
-  storageBucket: "studio-4910049931-ac9b9.appspot.com",
+  storageBucket: "studio-4910049931-ac9b9.firebasestorage.app",
   messagingSenderId: "1031434585972",
   appId: "1:1031434585972:web:316bca15751e1806530e21",
   measurementId: ""
